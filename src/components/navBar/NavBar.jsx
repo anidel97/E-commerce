b72e462b6b13951486521f9eb58bc7e1f7fc892e
Anahí Delgado <anidel97@hotmail.com>
2025-04-05 19:19:21 -0300

    Resolución de errores de imports

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -12,7 +12,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import CartWidget from '../cartWidget/CartWidget';
 import { NavLink, Link } from 'react-router-dom';
-import styles from './NavBar.module.css';
+import styles from './navBar.module.css';
 
 
 function ResponsiveAppBar() {
